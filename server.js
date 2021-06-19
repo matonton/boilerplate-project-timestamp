b2918430d94a2c495ad96e78202bdd1002917dab
matonton <matonton@yahoo.com>
2021-06-19 06:35:47 +0100

    updated route params for api

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/:date", function (req, res) {
+  console.log(req.params.date);
   res.json({greeting: 'hello API'});
 });
 
