ce6a4c3a0918a3435f8c692a3e98a6fe365470b1
matonton <30802662+matonton@users.noreply.github.com>
2021-06-21 07:51:10 +0000

    fixed utc and unix results for both paths

--- server.js
+++ server.js
@@ -20,12 +20,28 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/:date", function (req, res) {
-  console.log(req.params.date);
-  res.json({greeting: 'hello API'});
+app.get("/api/:year-:month-:date", function (req, res) {
+  var y = Number(req.params.year);
+  var m = Number(req.params.month) - 1;
+  var d = Number(req.params.date);
+  var date = new Date(y, m, d);
+  
+  
+  var unix = date.getTime();
+  var utc = date.toUTCString();
+  res.json({ unix: unix, utc: utc });
 });
 
-
+// your first API endpoint... 
+app.get("/api/:unix", function (req, res) {
+  console.log(req.params.unix);
+  var date = new Date(Number(req.params.unix));
+  console.log(date);
+  var unix = date.getTime();
+  var utc = date.toUTCString();
+  console.log(utc);
+  res.json({ unix: unix, utc: utc });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
